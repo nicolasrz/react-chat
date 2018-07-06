bfd0c672fe054500fd35a932fcf968ae4251d82c
brandonsueur <gynidark@gmail.com>
2018-07-06 16:07:30 +0200

    feat: import glamor

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { css } from 'glamor'
+
+// make css rules
+const title = css({
+  fontFamily: 'Montserrat',
+  fontWeight: 100,
+  fontSize: '4em',
+  color: '#c3c3c3',
+  textAlign: 'center',
+  marginTop: '6em'
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Goosta</h1>
-      </div>
-    );
-  }
+  render = () => (<h1 {...title}>Goosta</h1>);
 }
 
 export default App;
